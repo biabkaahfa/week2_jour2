a958d339817dbb6fa3d457c41b35d6f4d26a4d09
Ouedraogo-develop <152028835+Ouedraogo-develop@users.noreply.github.com>
2024-06-18 23:07:16 +0000

    restore

--- exo_07.rb
+++
@@ -1,7 +0,0 @@
-puts "Bonjour, c'est quoi ton blase ?"
-
-user_name = gets.chomp
-
-puts user_name
-#c'est un programme qui qui te pose une question et te demande de saisir quelque chose et une fois saisie il te renvoie le mot saisie
-#Pour notre comprehension "gets.champ" renvoie le saisie.
