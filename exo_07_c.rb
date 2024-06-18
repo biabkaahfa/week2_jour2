0d25756507216b37dfe98805e0668b82340039df
Ouedraogo-develop <152028835+Ouedraogo-develop@users.noreply.github.com>
2024-06-18 23:05:49 +0000

    add new file

---
+++ exo_07_c.rb
@@ -0,0 +1,6 @@
+user_name = gets.chomp
+
+puts user_name
+#la difference des trois est que le premier pose une question en attendant une reponse par ecrit 
+#le deuxieme pose aussi une question mais il attend a ce que l'utilisateur envoie la reponse sous forme invite de commande
+#le troisieme ne pose pas de question mais si tu ecrit quelque chose il te renvoie ce que tu a saisie
